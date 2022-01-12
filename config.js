1448deb0b97c388d662d67c747e0ba467064bd81
LeTri234 <73783978+LeTri234@users.noreply.github.com>
2022-01-13 04:10:07 +0700

    Detele configjs

--- config.js
+++
@@ -1,5 +0,0 @@
-export const nftmarketaddress = "0x633F6BE30d7B3076052484EB30802e008B807Ce3";
-export const nftaddress = "0x3e7e9CffabBFeF09e1EB576eFf82B61A17eC6054";
-
-export const rpcEndpoint =
-  "https://polygon-mumbai.g.alchemy.com/v2/qcR8xIgSVVlUssw-d_aCblYqOdqvV66L";
